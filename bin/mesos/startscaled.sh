b1762d79286f880323cdfcb9368b45d6c4033e35
aspyker <awspyker@gmail.com>
2014-07-21 21:25:18 +0000

    script fixes

--- bin/mesos/startscaled.sh
+++ bin/mesos/startscaled.sh
@@ -4,16 +4,16 @@ CASSANDRAHOSTNAME=$1
 EUREKAHOSTNAME=$2
 
 # Start auth-service
-echo curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
+curl --verbose -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
     localhost:8080/v2/apps \
-    -d "{\"id\": \"mesos-acmeair-auth-service\", \"cmd\": \"docker run -e \"CASSANDRAHOSTNAME=${CASSANDRAHOSTNAME}\" -e \"EUREKAHOSTNAME=${EUREKAHOSTNAME}\" aspyker/mesos-acmeair-auth-service\", \"instances\": 1, \"mem\": 128, \"cpus\": 0.25}"
+    -d "{\"id\": \"mesos-acmeair-auth-service\", \"cmd\": \"docker run -e \\\"CASSANDRAHOSTNAME=${CASSANDRAHOSTNAME}\\\" -e \\\"EUREKAHOSTNAME=${EUREKAHOSTNAME}\\\" aspyker/mesos-acmeair-auth-service\", \"instances\": 1, \"mem\": 128, \"cpus\": 0.25}"
 
 # Start webapp
-echo curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
+curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
     localhost:8080/v2/apps \
-    -d "{\"id\": \"mesos-acmeair-webapp\", \"cmd\": \"docker run -e \"CASSANDRAHOSTNAME=${CASSANDRAHOSTNAME}\" -e \"EUREKAHOSTNAME=${EUREKAHOSTNAME}\" aspyker/mesos-acmeair-webapp\", \"instances\": 1, \"mem\": 128, \"cpus\": 0.25}"
+    -d "{\"id\": \"mesos-acmeair-webapp\", \"cmd\": \"docker run -e \\\"CASSANDRAHOSTNAME=${CASSANDRAHOSTNAME}\\\" -e \\\"EUREKAHOSTNAME=${EUREKAHOSTNAME}\\\" aspyker/mesos-acmeair-webapp\", \"instances\": 1, \"mem\": 128, \"cpus\": 0.25}"
 
 # Start zuul
-echo curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
+curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
     localhost:8080/v2/apps \
-    -d "{\"id\": \"mesos-acmeair-zuul\", \"cmd\": \"docker run -e \"EUREKAHOSTNAME=${EUREKAHOSTNAME}\" aspyker/mesos-acmeair-auth-service\", \"instances\": 1, \"mem\": 128, \"cpus\": 0.25}"
+    -d "{\"id\": \"mesos-acmeair-zuul\", \"cmd\": \"docker run -e \\\"EUREKAHOSTNAME=${EUREKAHOSTNAME}\\\" aspyker/mesos-acmeair-auth-service\", \"instances\": 1, \"mem\": 128, \"cpus\": 0.25}"
--- bin/mesos/startsingletons.sh
+++ bin/mesos/startsingletons.sh
@@ -3,14 +3,14 @@
 # Start cassandra
 curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
     localhost:8080/v2/apps \
-    -d '{"id": "mesos-acmeair-cassandra", "cmd": "docker run aspyker/mesos-acmeair-cassandra", "instances": 1, "mem": 128, "cpus": 0.25}'
+    -d '{"id": "mesos-acmeair-cassandra", "cmd": "docker run -name cassandra aspyker/mesos-acmeair-cassandra", "instances": 1, "mem": 128, "cpus": 0.25}'
 
 # Start loader
 curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
     localhost:8080/v2/apps \
-    -d '{"id": "mesos-acmeair-loader", "cmd": "docker run aspyker/mesos-acmeair-loader", "instances": 1, "mem": 128, "cpus": 0.25}'
+    -d '{"id": "mesos-acmeair-loader", "cmd": "docker run -name loader aspyker/mesos-acmeair-loader", "instances": 1, "mem": 128, "cpus": 0.25}'
 
 # Start eureka
 curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
     localhost:8080/v2/apps \
-    -d '{"id": "mesos-acmeair-eureka", "cmd": "docker run aspyker/mesos-acmeair-eureka", "instances": 1, "mem": 128, "cpus": 0.25}'
+    -d '{"id": "mesos-acmeair-eureka", "cmd": "docker run -name eureka aspyker/mesos-acmeair-eureka", "instances": 1, "mem": 128, "cpus": 0.25}'
