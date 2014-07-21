c9a5e8e01b6374294564b5002cc18621a73dbcfc
aspyker <awspyker@gmail.com>
2014-07-21 21:42:29 +0000

    fixes to startscaled

--- bin/mesos/startscaled.sh
+++ bin/mesos/startscaled.sh
@@ -4,16 +4,16 @@ CASSANDRAHOSTNAME=$1
 EUREKAHOSTNAME=$2
 
 # Start auth-service
-curl --verbose -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
+curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
     localhost:8080/v2/apps \
-    -d "{\"id\": \"mesos-acmeair-auth-service\", \"cmd\": \"docker run -e \\\"CASSANDRAHOSTNAME=${CASSANDRAHOSTNAME}\\\" -e \\\"EUREKAHOSTNAME=${EUREKAHOSTNAME}\\\" aspyker/mesos-acmeair-auth-service\", \"instances\": 1, \"mem\": 128, \"cpus\": 0.25}"
+    -d "{\"id\": \"mesos-acmeair-auth-service\", \"cmd\": \"docker run -e \\\"CASSANDRAHOSTNAME=${CASSANDRAHOSTNAME}\\\" -e \\\"EUREKAHOSTNAME=${EUREKAHOSTNAME}\\\" aspyker/mesos-acmeair-auth-service\", \"instances\": 2, \"mem\": 128, \"cpus\": 0.25}"
 
 # Start webapp
 curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
     localhost:8080/v2/apps \
-    -d "{\"id\": \"mesos-acmeair-webapp\", \"cmd\": \"docker run -e \\\"CASSANDRAHOSTNAME=${CASSANDRAHOSTNAME}\\\" -e \\\"EUREKAHOSTNAME=${EUREKAHOSTNAME}\\\" aspyker/mesos-acmeair-webapp\", \"instances\": 1, \"mem\": 128, \"cpus\": 0.25}"
+    -d "{\"id\": \"mesos-acmeair-webapp\", \"cmd\": \"docker run -e \\\"CASSANDRAHOSTNAME=${CASSANDRAHOSTNAME}\\\" -e \\\"EUREKAHOSTNAME=${EUREKAHOSTNAME}\\\" aspyker/mesos-acmeair-webapp\", \"instances\": 2, \"mem\": 128, \"cpus\": 0.25}"
 
 # Start zuul
 curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
     localhost:8080/v2/apps \
-    -d "{\"id\": \"mesos-acmeair-zuul\", \"cmd\": \"docker run -e \\\"EUREKAHOSTNAME=${EUREKAHOSTNAME}\\\" aspyker/mesos-acmeair-auth-service\", \"instances\": 1, \"mem\": 128, \"cpus\": 0.25}"
+    -d "{\"id\": \"mesos-acmeair-zuul\", \"cmd\": \"docker run -p=80:80 -e \\\"EUREKAHOSTNAME=${EUREKAHOSTNAME}\\\" aspyker/mesos-acmeair-zuul\", \"instances\": 1, \"mem\": 128, \"cpus\": 0.25}"
